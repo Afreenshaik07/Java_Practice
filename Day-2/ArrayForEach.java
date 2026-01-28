d2bb4ae2ce339c71fd1a2e873db49a08e4d48d6f
Afreenshaik07 <afreenshaik0711@gmail.com>
2026-01-28 19:08:16 +0530

    Day 2: array practice programs

---
+++ Day-2/ArrayForEach.java
@@ -0,0 +1,9 @@
+public class ArrayForEach {
+    public static void main(String[] args) {
+        int[] arr = {5, 15, 25, 35};
+
+        for(int num : arr) {
+            System.out.println(num);
+        }
+    }
+}
