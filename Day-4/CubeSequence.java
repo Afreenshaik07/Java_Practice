84b8ae2f6ad9df38351e5fbe5f8ea3b6c2ab12fd
Afreenshaik07 <afreenshaik0711@gmail.com>
2026-01-30 18:34:27 +0530

    Day 4: Number Sequence

---
+++ Day-4/CubeSequence.java
@@ -0,0 +1,9 @@
+public class CubeSequence {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(i * i * i + " ");
+        }
+    }
+}
---
+++ Day-4/SquareSequence.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class SquareSequence {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            int n = sc.nextInt();
+
+            for (int i = 1; i <= n; i++) {
+                System.out.print(i * i + " ");
+            }
+        }
+    }
+}
